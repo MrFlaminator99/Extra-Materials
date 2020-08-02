eeec2614eacef8b42aae91325e5ae5595c4022f9
MrFlaminator99 <69107744+MrFlaminator99@users.noreply.github.com>
2020-08-02 12:59:09 +0200

    Add files via upload

---
+++ lua/autorun/rawskellz.additionalmaterials.lua
@@ -0,0 +1,10 @@
+-- Additional Materials by RAWSKELLZ
+-- vk.com/rawskellz
+-- Addon for GMOD13 and above.
+
+list.Add( "OverrideMaterials", "lights/white" ) --Glow
+list.Add( "OverrideMaterials", "effects/combineshield/comshieldwall" ) --Energy Field
+list.Add( "OverrideMaterials", "sprites/heatwave" ) --Heat Wave
+list.Add( "OverrideMaterials", "models/shadertest/predator" ) -- Water
+list.Add( "OverrideMaterials", "models/props/de_inferno/offwndwb_break" ) -- Broken Glass
+list.Add( "OverrideMaterials", "models/player/shared/gold_player" ) -- TF2 Gold
